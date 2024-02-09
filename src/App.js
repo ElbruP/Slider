ff2c4d623b4c3b1cbf0186fd2c4efb275b290c13
Elbrus <136369048+ElbruP@users.noreply.github.com>
2024-02-09 21:09:55 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import SimpleSlider from './components/Slider';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleSlider />
+    </div>
+  );
+}
+
+export default App;
